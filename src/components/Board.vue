fc396b9824d59068545f9213fab02b6b3c1d4579
kerkkerk12 <6210742356@student.tu.ac.th>
2022-12-01 23:09:52 +0700

    set log player and rm first block

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="container">
-    <h2 v-if="winner">Winner: {{ winner }}</h2>
-    <h2 v-else>Player Move: {{ player }}</h2>
+    <h2 v-if="winner">the Winner is : {{ winner }} score plus {{blockLeft}}</h2>
+    <h2 v-else-if="player ==='-' ">Press the first block to remove.</h2>
+    <h2 v-else-if="blockLeft === 0 ">The game is end no player win.</h2>
+    <h2 v-else >Player "{{ player }}" move.</h2>
     <button @click="reset" class="btn btn-success mb-3">Reset</button>
     <div v-for="(z, x) in 4" :key="x" class="row">
       <button v-for="(z, y) in 4" :key="y" @click="move(x, y)" class="square">
@@ -26,8 +28,8 @@
   font-weight: bold;
   line-height: 34px;
   height: 100px;
-  margin-right: -1px;
-  margin-top: -1px;
+  margin-right: 2px;
+  margin-top: 2px;
   padding: 0;
   text-align: center;
   width: 100px;
@@ -37,6 +39,7 @@
 .score {
   margin: auto;
 }
+
 </style>
 <!-- ///////////////////////////////////////////// -->
 <script>
@@ -69,7 +72,7 @@ const Winner = (squares) => {
 export default {
   setup() {
     const blockLeft = ref(16);
-    const player = ref("X");
+    const player = ref("-");
     const squares = ref([
       ["", "", "", ""],
       ["", "", "", ""],
@@ -81,15 +84,18 @@ export default {
       if (winner.value) return;
 
       if (squares.value[x][y] !== "") return;
+        
+      
+
       squares.value[x][y] = player.value;
-      player.value = player.value === "O" ? "X" : "O";
+      player.value = player.value === "X" ? "O" : "X";
       blockLeft.value -= 1;
       console.log(blockLeft);
     };
 
     const reset = () => {
       blockLeft.value = 16;
-      player.value = "X";
+      player.value = "-";
       squares.value = [
         ["", "", "", ""],
         ["", "", "", ""],
